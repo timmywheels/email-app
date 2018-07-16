3ab1b5bed9e9eed8e2ee541905d89ab99c1bd085
Tim Wheeler <tim@timwheeler.com>
2018-07-15 23:52:19 -0400

    updated keys

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+  google: {
+    clientId:
+      '540883463967-1r558el0ot5joqtlkrvlme7sv8pmh190.apps.googleusercontent.com',
+    clientSecret: 'PStfBqScG3osK1DjJEW-W-iA'
+  },
+  mongo: {
+    user: encodeURIComponent('email-app-dev'),
+    pass: encodeURIComponent('TWheeler91$'),
+    uri: '@ds137581.mlab.com:37581/email-app-dev'
+  }
+};
