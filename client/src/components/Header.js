d9d12396f2069c9b67f1092143c65ee116947570
Tim Wheeler <tim@timwheeler.com>
2018-07-26 23:52:09 -0400

    added payment gateway

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,51 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
-	render() {
-		return (
-			<nav>
-				<div className="nav-wrapper">
-					<a className="left brand-logo">Email App</a>
-					<ul className="right">
-						<li>
-							<a href="/auth/google">Login With Google</a>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		);
-	}
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return [
+          <li key={'1'}>
+            <Payments />
+          </li>,
+          <li key={'2'}>
+            <a href="/api/logout">Logout</a>
+          </li>
+        ];
+    }
+  }
+
+  render() {
+    console.log(this.props.auth);
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo"
+          >
+            Email App
+          </Link>
+          <ul className="right">{this.renderContent()}</ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
