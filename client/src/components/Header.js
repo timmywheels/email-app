6692b5c4f0f1d7be7de6222f1342ba6b9d0c81a9
Tim <tim@timwheeler.com>
2018-07-29 14:15:15 -0400

    added billing and client side app

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -19,7 +19,7 @@ class Header extends Component {
 					<li key={'1'}>
 						<Payments />
 					</li>,
-					<li key={'2'} style={{ margin: '0 10px' }}>
+					<li key={'2'} style={{ margin: '0 15px' }}>
 						Credits: {this.props.auth.credits}
 					</li>,
 					<li key={'3'}>
