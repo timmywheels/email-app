71057b17c199765c5272303826c48cba4002da7e
Tim Wheeler <tim@timwheeler.com>
2018-07-28 23:05:57 -0400

    edited Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -35,7 +35,7 @@ class Header extends Component {
             to={this.props.auth ? '/surveys' : '/'}
             className="left brand-logo"
           >
-            Email App
+            Email
           </Link>
           <ul className="right">{this.renderContent()}</ul>
         </div>
